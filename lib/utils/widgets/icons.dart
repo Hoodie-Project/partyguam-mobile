1e2f6311da6838d1e6cd634d1425a313941d9cc8
ha-ccoon <codeminhasohn@gmail.com>
2024-03-02 02:00:45 +0900

    chore: create a custom icons - in process

---
+++ lib/utils/widgets/icons.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+
+class ClearCircleIcon extends StatelessWidget {
+  const ClearCircleIcon({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Image.asset(
+      'assets/icons/clear_circle.png',
+      width: 24,
+      height: 24,
+    );
+  }
+}
