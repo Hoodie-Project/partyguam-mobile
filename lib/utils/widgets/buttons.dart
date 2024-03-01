fbf7dc0d8406289d20b7dd9f497933f9d62546ee
ha-ccoon <codeminhasohn@gmail.com>
2024-03-01 11:46:50 +0900

    chore: create a longHorizontalButton

--- lib/utils/widgets/buttons.dart
+++ lib/utils/widgets/buttons.dart
@@ -1,26 +1,20 @@
+import 'dart:ui';
+
 import 'package:flutter/material.dart';
 import 'package:partyguam/utils/theme/color.dart';
 
-class LongHorizontalButton extends StatelessWidget {
-  final String content;
-
-  const LongHorizontalButton({super.key, required this.content});
-
-  @override
-  Widget build(BuildContext context) {
-    return ElevatedButton(
-      onPressed: null,
-      style: ButtonStyle(
-          backgroundColor: MaterialStatePropertyAll(AppColors.primaryColors),
-          textStyle: MaterialStatePropertyAll(
-            TextStyle(
-              fontSize: 14.0,
-              fontWeight: FontWeight.w700,
-              // color: AppColors.greyColors.shade700,
-            ),
-          ),
-  
-      child: Text(content),
-    );
-  }
-}
+final ButtonStyle longHorizontal = ElevatedButton.styleFrom(
+  backgroundColor: AppColors.primaryColors,
+  minimumSize: const Size(335, 48),
+  elevation: 0,
+  shape: const RoundedRectangleBorder(
+    borderRadius: BorderRadius.all(
+      Radius.circular(12)
+    )
+  ),
+  textStyle: TextStyle(
+    color: AppColors.greyColors.shade700,
+    fontSize: 14.0,
+    fontWeight: FontWeight.w700,
+  )
+);
