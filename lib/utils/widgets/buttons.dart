cda3a431831e5bb46fe02ccb3548780226827e53
ha-ccoon <codeminhasohn@gmail.com>
2024-03-03 12:45:34 +0900

    chore: create a squareButton

--- lib/utils/widgets/buttons.dart
+++ lib/utils/widgets/buttons.dart
@@ -1,52 +1,87 @@
-import 'dart:ui';
-
 import 'package:flutter/material.dart';
 import 'package:partyguam/utils/theme/color.dart';
 
 final ButtonStyle longHorizontal = ElevatedButton.styleFrom(
-  backgroundColor: AppColors.primaryColors,
+  backgroundColor: AppColors.primaryLightColors,
   minimumSize: const Size(335, 48),
   elevation: 0,
   shape: const RoundedRectangleBorder(
     borderRadius: BorderRadius.all(
-      Radius.circular(12)
-    )
+      Radius.circular(12),
+    ),
   ),
   textStyle: TextStyle(
     color: AppColors.greyColors.shade700,
     fontSize: 14.0,
     fontWeight: FontWeight.w700,
-  )
+  ),
 );
 
 final ButtonStyle mainShortHorizontal = ElevatedButton.styleFrom(
-    backgroundColor: AppColors.primaryColors,
-    minimumSize: const Size(164, 48),
-    elevation: 0,
-    shape: const RoundedRectangleBorder(
-        borderRadius: BorderRadius.all(
-            Radius.circular(12)
-        )
+  backgroundColor: AppColors.primaryLightColors,
+  minimumSize: const Size(164, 48),
+  elevation: 0,
+  shape: const RoundedRectangleBorder(
+    borderRadius: BorderRadius.all(
+      Radius.circular(12),
     ),
-    textStyle: TextStyle(
-      color: AppColors.greyColors.shade700,
-      fontSize: 14.0,
-      fontWeight: FontWeight.w700,
-    )
+  ),
+  textStyle: TextStyle(
+    color: AppColors.greyColors.shade700,
+    fontSize: 14.0,
+    fontWeight: FontWeight.w700,
+  ),
 );
 
 final ButtonStyle subShortHorizontal = ElevatedButton.styleFrom(
-    side: const BorderSide(color: AppColors.primaryColors),
-    minimumSize: const Size(164, 48),
-    elevation: 0,
-    shape: const RoundedRectangleBorder(
-        borderRadius: BorderRadius.all(
-            Radius.circular(12)
-        )
+  side: const BorderSide(color: AppColors.primaryLightColors),
+  minimumSize: const Size(164, 48),
+  elevation: 0,
+  shape: const RoundedRectangleBorder(
+    borderRadius: BorderRadius.all(
+      Radius.circular(12),
     ),
-    textStyle: TextStyle(
-      color: AppColors.greyColors.shade700,
-      fontSize: 14.0,
-      fontWeight: FontWeight.w700,
-    )
+  ),
+  textStyle: TextStyle(
+    color: AppColors.greyColors.shade700,
+    fontSize: 14.0,
+    fontWeight: FontWeight.w700,
+  ),
 );
+
+class SquareButton extends StatefulWidget {
+  final String content;
+
+  const SquareButton({super.key, required this.content});
+
+  @override
+  State<SquareButton> createState() => _SquareButtonState();
+}
+
+class _SquareButtonState extends State<SquareButton> {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      width: 163,
+      height: 163,
+      decoration: BoxDecoration(
+        border: Border.all(
+          color: AppColors.greyColors.shade200,
+          width: 1.0,
+        ),
+        borderRadius: const BorderRadius.all(
+          Radius.circular(16),
+        ),
+      ),
+      child: Center(
+        child: Text(
+          widget.content,
+          style: const TextStyle(
+            fontSize: 16.0,
+            fontWeight: FontWeight.normal,
+          ),
+        ),
+      ),
+    );
+  }
+}
