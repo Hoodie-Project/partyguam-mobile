8907e03302897ad2f7c4a5359650628372a52f4a
ha-ccoon <codeminhasohn@gmail.com>
2024-03-05 01:21:45 +0900

    chore: implement a custom fontstyle

---
+++ lib/utils/theme/font_style.dart
@@ -0,0 +1,20 @@
+import 'package:flutter/material.dart';
+import 'package:partyguam/utils/theme/color.dart';
+
+class CustomFontStyle {
+  static const TextStyle loginMainTitle = TextStyle(
+    fontSize: 18.0,
+    fontWeight: FontWeight.w700,
+  );
+
+  static const TextStyle loginSubTitle = TextStyle(
+    fontSize: 14.0,
+    fontWeight: FontWeight.normal,
+  );
+
+  static TextStyle validationErrorMessage = TextStyle(
+    fontSize: 12.0,
+    fontWeight: FontWeight.normal,
+    color: AppColors.systemColors.shade100,
+  );
+}
