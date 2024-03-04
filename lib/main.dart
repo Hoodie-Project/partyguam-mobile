107e4fc0a362ae766b11d13591bea793fa766a48
ha-ccoon <codeminhasohn@gmail.com>
2024-03-05 07:45:12 +0900

    chore: commit to save in progress
    - at work

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,10 @@
 import 'package:flutter/material.dart';
+import 'package:partyguam/app/pages/sign_up/login_0000.dart';
+import 'package:partyguam/app/pages/sign_up/register_0111.dart';
+import 'package:partyguam/utils/theme/app_bar.dart';
+import 'package:partyguam/utils/theme/theme.dart';
+import 'package:partyguam/utils/widgets/buttons.dart';
+import 'package:partyguam/utils/widgets/inputs.dart';
 
 void main() {
   runApp(const MyApp());
@@ -12,41 +18,19 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'Flutter Demo',
-      theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // TRY THIS: Try running your application with "flutter run". You'll see
-        // the application has a purple toolbar. Then, without quitting the app,
-        // try changing the seedColor in the colorScheme below to Colors.green
-        // and then invoke "hot reload" (save your changes or press the "hot
-        // reload" button in a Flutter-supported IDE, or press "r" if you used
-        // the command line to start the app).
-        //
-        // Notice that the counter didn't reset back to zero; the application
-        // state is not lost during the reload. To reset the state, use hot
-        // restart instead.
-        //
-        // This works for code too, not just values: Most code changes can be
-        // tested with just a hot reload.
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
-        useMaterial3: true,
-      ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+      theme: lightTheme,
+      home: const MyHomePage(title: '로그인'),
+      initialRoute: '/',
+      routes: {
+        '/login': (context) => const Login(),
+        '/register_0111': (context) => const Registration011(),
+      },
     );
   }
 }
 
 class MyHomePage extends StatefulWidget {
   const MyHomePage({super.key, required this.title});
-
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
 
   final String title;
 
@@ -55,15 +39,11 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
+  final formKey = GlobalKey<FormState>();
   int _counter = 0;
 
   void _incrementCounter() {
     setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
       _counter++;
     });
   }
@@ -77,34 +57,59 @@ class _MyHomePageState extends State<MyHomePage> {
     // fast, so that you can just rebuild anything that needs updating rather
     // than having to individually change instances of widgets.
     return Scaffold(
-      appBar: AppBar(
-        // TRY THIS: Try changing the color here to a specific color (to
-        // Colors.amber, perhaps?) and trigger a hot reload to see the AppBar
-        // change color while the other colors stay the same.
-        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
-        title: Text(widget.title),
+      appBar: const SignUpAppBar(
+        title: '로그인',
       ),
+
       body: Center(
         // Center is a layout widget. It takes a single child and positions it
         // in the middle of the parent.
         child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          //
-          // TRY THIS: Invoke "debug painting" (choose the "Toggle Debug Paint"
-          // action in the IDE, or press "p" in the console), to see the
-          // wireframe for each widget.
           mainAxisAlignment: MainAxisAlignment.center,
           children: <Widget>[
+            SizedBox(
+              height: 2,
+            ),
+            ElevatedButton(
+              onPressed: () {
+                Navigator.pushNamed(context, '/login');
+              },
+              style: longHorizontal,
+              child: const Text('다음'),
+            ),
+            SizedBox(
+              height: 2,
+            ),
+            ElevatedButton(
+              onPressed: () {
+                Navigator.pushNamed(context, '/login');
+              },
+              style: mainShortHorizontal,
+              child: const Text('작성하기'),
+            ),
+            SizedBox(
+              height: 2,
+            ),
+            ElevatedButton(
+              onPressed: () {
+                Navigator.pushNamed(context, '/login');
+              },
+              style: subShortHorizontal,
+              child: const Text('홈으로'),
+            ),
+            SizedBox(
+              height: 2,
+            ),
+            Form(
+                key: formKey,
+                child: Column(
+                  children: [
+                    PrimaryInputBox(
+                      hintText: '20자 이상',
+                    )
+                  ],
+                )),
+            SquareButton(content: '여자'),
             const Text(
               'You have pushed the button this many times:',
             ),
--- lib/utils/theme/theme.dart
+++ lib/utils/theme/theme.dart
@@ -1,18 +1,19 @@
 import 'package:flutter/material.dart';
+import 'package:partyguam/utils/theme/color.dart';
 
 final ThemeData lightTheme = ThemeData(
-  brightness: Brightness.light,
-  useMaterial3: true,
-  appBarTheme: const AppBarTheme(
+  appBarTheme: AppBarTheme(
     backgroundColor: Colors.white,
     elevation: 0,
     titleTextStyle: TextStyle(
-      color: Colors.black,
+      color: AppColors.greyColors.shade700,
       fontSize: 16,
       fontFamily: 'SUIT',
       fontWeight: FontWeight.w700,
     ),
     centerTitle: true,
   ),
+  brightness: Brightness.light,
+  fontFamily: 'SUIT',
+  useMaterial3: true,
 );
-
