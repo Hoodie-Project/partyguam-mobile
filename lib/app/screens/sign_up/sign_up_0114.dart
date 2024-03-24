13edb0c2c0065c15cf55a651cc48052b5c624b92
ha-ccoon <codeminhasohn@gmail.com>
2024-03-25 08:31:20 +0900

    chore: implement the GestureDetector to the square buttons
    - SignUp0114

--- lib/app/screens/sign_up/sign_up_0114.dart
+++ lib/app/screens/sign_up/sign_up_0114.dart
@@ -2,26 +2,52 @@ import 'package:flutter/material.dart';
 import 'package:partyguam/app/screens/sign_up/widgets/app_bar.dart';
 import 'package:partyguam/app/screens/sign_up/widgets/buttons.dart';
 import 'package:partyguam/app/screens/sign_up/widgets/text.dart';
+import 'package:partyguam/app/theme/colors.dart';
 
-class SignUp0114 extends StatelessWidget {
+class SignUp0114 extends StatefulWidget {
   const SignUp0114({super.key});
 
   @override
+  State<SignUp0114> createState() => _SignUp0114State();
+}
+
+class _SignUp0114State extends State<SignUp0114> {
+  Color _containerColor1 = AppColors.greyColors.shade50;
+  Color containerColor2 = AppColors.greyColors.shade50;
+  Color tappedColor = AppColors.primaryLightColors.shade300;
+
+  void _changeColor1() {
+    setState(() {
+      _containerColor1 =
+          _containerColor1 == tappedColor ? _containerColor1 : tappedColor;
+      containerColor2 = AppColors.greyColors.shade50;
+    });
+  }
+
+  void _changeColor2() {
+    setState(() {
+      containerColor2 =
+          containerColor2 == tappedColor ? containerColor2 : tappedColor;
+      _containerColor1 = AppColors.greyColors.shade50;
+    });
+  }
+
+  @override
   Widget build(BuildContext context) {
-    return const Scaffold(
-      appBar: SignUpAppBar(
+    return Scaffold(
+      appBar: const SignUpAppBar(
         title: '가입하기',
         route: '/sign_up/0115',
         pageCount: '4/4',
       ),
       body: Padding(
-        padding:
-            EdgeInsets.only(left: 20.0, top: 40.0, right: 20.0, bottom: 12.0),
+        padding: const EdgeInsets.only(
+            left: 20.0, top: 40.0, right: 20.0, bottom: 12.0),
         child: Center(
           child: Column(
             crossAxisAlignment: CrossAxisAlignment.start,
             children: [
-              TitleText(
+              const TitleText(
                 mainTitle: '성별은\n어떻게 되시나요?',
                 subTitle: '프로필에서 노출 여부를 설정할 수 있어요.',
               ),
@@ -29,20 +55,36 @@ class SignUp0114 extends StatelessWidget {
                 mainAxisAlignment: MainAxisAlignment.spaceBetween,
                 children: [
                   Expanded(
-                    child: SquareButton(content: '남자'),
+                    child: GestureDetector(
+                      onTap: () {
+                        _changeColor1();
+                      },
+                      child: SquareButton(
+                        content: '남자',
+                        containerColor: _containerColor1,
+                      ),
+                    ),
                   ),
-                  SizedBox(
+                  const SizedBox(
                     width: 8.0,
                   ),
                   Expanded(
-                    child: SquareButton(content: '여자'),
+                    child: GestureDetector(
+                      onTap: () {
+                        _changeColor2();
+                      },
+                      child: SquareButton(
+                        content: '여자',
+                        containerColor: containerColor2,
+                      ),
+                    ),
                   ),
                 ],
               ),
-              Expanded(
+              const Expanded(
                 child: SizedBox(),
               ),
-              MainHorizontalButton(
+              const MainHorizontalButton(
                 content: '다음',
                 route: '/sign_up/0115',
               ),
